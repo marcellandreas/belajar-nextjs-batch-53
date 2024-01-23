c71f86503cd9a9e26ee3b97c1c1186b1a3017e78
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2024-01-24 00:59:10 +0700

    feat: Tugas Harian ke-7: pages routing

--- components/menu/index.js
+++ components/menu/index.js
@@ -1,30 +1,36 @@
+import Link from "next/link";
+
 export default function Menu({ isOpen }) {
+  // sebelumnya saya pernah membuat pages routing
+  const menu = [
+    {
+      id: 1,
+      name: "Home",
+      href: "/",
+    },
+    {
+      id: 2,
+      name: "profile",
+      href: "/profile",
+    },
+    {
+      id: 3,
+      name: "blog",
+      href: "/blog",
+    },
+    {
+      id: 4,
+      name: "contact",
+      href: "/contact",
+    },
+  ];
   return (
     <div className="flex items-center gap-3 text-xl">
-      <a
-        href="/"
-        className=" hover:underline hover:text-gray-400 font-semibold"
-      >
-        Home
-      </a>
-      <a
-        href="/profile"
-        className=" hover:underline hover:text-gray-400 font-semibold"
-      >
-        Profile
-      </a>
-      <a
-        href="/blog"
-        className=" hover:underline hover:text-gray-400 font-semibold"
-      >
-        Blog
-      </a>
-      <a
-        href="/contact"
-        className=" hover:underline hover:text-gray-400 font-semibold"
-      >
-        Contact
-      </a>
+      {menu.map((data) => (
+        <Link href={data.href} key={data.id}>
+          {data.name}
+        </Link>
+      ))}
     </div>
   );
 }
